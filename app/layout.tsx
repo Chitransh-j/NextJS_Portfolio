8afa65dccc3545a0c625c39beab5e289a4932b38
Chitransh-j <chitranshjawere3@gmail.com>
2024-02-10 08:18:19 +0530

    first draft

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import Header from '@/components/Header'
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: "Chitransh's Portfolio",
+  description: 'An Aspiring Full Stack Developer with sharp Problem solving Skills and grasp at DSA.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-slate-200 text-gray-950 relative h-[5000px] pt-28 sm:pt-35`}>
+        <div className='bg-[#fbe2e3] -z-10 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] 
+        sm:w-[68.75rem]'></div>
+        <div className='bg-[#dbd7fb] -z-10 absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] 
+        sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]' ></div>
+
+        <Header />
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ components/intro.tsx
@@ -0,0 +1,61 @@
+"use client" ; 
+import Image from 'next/image'
+import React from 'react'
+import {motion} from "framer-motion"
+import Link from 'next/link';
+import {BsArrowRight, BsGithub, BsLinkedin} from "react-icons/bs"
+import {HiDownload} from 'react-icons/hi'
+const intro = () => {
+  return (
+    <section className='mb-28 max-w-[50rem] text-center sm:mb-0'>
+        <div className='flex items-center justify-center pb-[0.5rem]'>
+            <div className='relative'>
+                <motion.div 
+                    initial={{opacity:0 ,scale:0}}
+                    animate = {{opacity:1 ,scale:1}}
+                    transition={{
+                        type:"tween",
+                        duration:0.3,
+                        }}>
+                    <Image  width="250" height="250" quality="95" priority={true} src="https://i.pinimg.com/originals/59/9c/b9/599cb96a0adc21064c635d3de237fc04.jpg" alt="colt-pfp"
+                    className='h-36 w-30 rounded-full border-[0.35rem] border-white object-cover shadow-xl'/>
+                </motion.div>
+            </div>
+        </div>
+   
+        <motion.p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-2xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}>
+        <span className="font-bold">{`Hello, I'm Chitransh.`}</span> {`I'm an`}{" "}
+        <span className="font-bold">aspiring Full-Stack Developer</span> with{" "}
+        interest in Problem Solving and Algorithms. I also love 
+        building <span className="italic">Websites</span>. My focus is{" "}
+        <span className=''><span className='font-bold'>MERN</span> Stack</span>.
+        </motion.p>
+        
+        <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-3 px-4 text-lg font-medium'
+
+            initial={{opacity:0,y:100}}
+            animate={{opacity:1,y:0}}
+            transition={{
+                delay:0.1
+            }}>
+                                         {/* group hovering */}
+            <Link href="#contact" className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:scale-110 hover:bg-gray-950 transition-all'>Contact me here <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition '/></Link>
+      
+        <a className='group bg-white text-black px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:scale-110 transition-all cursor-pointer border border-black/10' href='/CV.pdf' download={true}>Download CV <HiDownload className='opacity-60 group-hover:translate-y-1 transition'/>
+        </a>
+
+        <a className='bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full hover:scale-[1.15] transition cursor-pointer border border-black/10' href="https://www.linkedin.com/in/chitransh-jawere-878497232/" target="_blank">
+            <BsLinkedin/>
+        </a>
+
+        <a className='bg-white text-gray-700 text-[1.35rem] p-4 flex items-center gap-2 rounded-full hover:scale-[1.15] transition cursor-pointer border border-black/10' href="https://github.com/Chitransh-j" target="_blank">
+            <BsGithub/>
+        </a>
+        </motion.div>
+    </section>
+  )
+}
+
+export default intro
