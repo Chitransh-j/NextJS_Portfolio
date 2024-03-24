4c59831de71a39ab0d6644c151ad59bbefe03426
Chitransh-j <chitranshjawere3@gmail.com>
2024-03-24 16:26:31 +0530

    updated cv and ratings

--- components/about.tsx
+++ components/about.tsx
@@ -1,7 +1,4 @@
 "use client";
-
-"use client";
-
 import React from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
@@ -16,29 +13,39 @@ export default function About() {
       className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ delay:0.175 }}
+      transition={{ delay: 0.175 }}
       id="about"
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        I&apos;m currently pursuing my <span className="font-medium">Bachelors of Technology</span> in <span className="italic">Information Technology</span>{" "}
-        at <br/> <span className="font-bold"><a className="" href="https://www.nitrr.ac.in/" target="_blank"> National Institute of Technology Raipur. {" "}</a></span>
-        I started learning <span className="font-medium"> Algorithms </span> and <span className="font-medium">Web Development</span>  as a part of the curriculum {" "}
-        and immediately fell in love with both of them.{" "}
-       My core stack
-        is{" "}
+        I&apos;m currently pursuing my{" "}
+        <span className="font-medium">Bachelors of Technology</span> in{" "}
+        <span className="italic">Information Technology</span> at <br />{" "}
+        <span className="font-bold">
+          <a className="" href="https://www.nitrr.ac.in/" target="_blank">
+            {" "}
+            National Institute of Technology Raipur.{" "}
+          </a>
+        </span>
+        I started learning <span className="font-medium">
+          {" "}
+          Algorithms{" "}
+        </span> and <span className="font-medium">Web Development</span> as a
+        part of the curriculum and immediately fell in love with both of them.{" "}
+        My core stack is{" "}
         <span className="font-medium">
           React, Next.js, Node.js, and Express.js
         </span>
-        . I am also familiar with TypeScript and Tailwind CSS. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
+        . I am also familiar with TypeScript and Tailwind CSS. I am always
+        looking to learn new technologies. I am currently looking for a{" "}
         <span className="font-medium">full-time position</span> as a software
         developer.
       </p>
 
       <p>
-         I am also 1740+ rated on <span className="font-semibold">LeetCode</span> and ⭐⭐⭐ at <span className="font-semibold">CodeChef</span>. <br/>{" "}
-        <span className="font-medium"></span>. 
+        I am also rated 1848 on <span className="font-semibold">LeetCode</span>{" "}
+        and ⭐⭐⭐ at <span className="font-semibold">CodeChef</span>. <br />{" "}
+        <span className="font-medium"></span>.
       </p>
     </motion.section>
   );
--- components/intro.tsx
+++ components/intro.tsx
@@ -4,69 +4,105 @@ import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { BsArrowRight, BsLinkedin,BsGithub} from "react-icons/bs";
+import { BsArrowRight, BsLinkedin, BsGithub } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
 
-
-
 const Intro = () => {
-    const { ref } = useSectionInView("Home", 0.5);
-    const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+  const { ref } = useSectionInView("Home", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
   return (
-    <section ref={ref} id="home" className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]'>
-        <div className='flex items-center justify-center pb-[0.5rem]'>
-            <div className='relative'>
-                <motion.div 
-                    initial={{opacity:0 ,scale:0}}
-                    animate = {{opacity:1 ,scale:1}}
-                    transition={{
-                        type:"tween",
-                        duration:0.3,
-                        }}>
-                    <Image  width="250" height="250" quality="95" priority={true} src="https://i.pinimg.com/originals/59/9c/b9/599cb96a0adc21064c635d3de237fc04.jpg" alt="colt-pfp"
-                    className='h-36 w-30 rounded-full border-[0.35rem] border-white object-cover shadow-xl'/>
-                </motion.div>
-            </div>
+    <section
+      ref={ref}
+      id="home"
+      className="mb-28 max-w-[100rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
+      <div className="flex items-center justify-center pb-[0.5rem]">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.3,
+            }}
+          >
+            <Image
+              width="250"
+              height="250"
+              quality="95"
+              priority={true}
+              src="https://i.pinimg.com/originals/59/9c/b9/599cb96a0adc21064c635d3de237fc04.jpg"
+              alt="colt-pfp"
+              className="h-36 w-30 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
+            />
+          </motion.div>
         </div>
-   
-        <motion.h1 className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-2xl"
+      </div>
+
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-2xl"
         initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}>
+        animate={{ opacity: 1, y: 0 }}
+      >
         <span className="font-bold">{`Hello, I'm Chitransh.`}</span> {`I'm an`}{" "}
         <span className="font-bold">aspiring Full-Stack Developer</span> with{" "}
-        interest in Problem Solving and Algorithms. I also love 
-        building <span className="italic">Websites</span>. My focus is{" "}
-        <span className=''><span className='font-bold'>MERN</span> Stack</span>.
-        </motion.h1>
-        
-        <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-3 px-4 text-lg font-medium'
+        interest in Problem Solving and Algorithms. I also love building{" "}
+        <span className="italic">Websites</span>. My focus is{" "}
+        <span className="">
+          <span className="font-bold">MERN</span> Stack
+        </span>
+        .
+      </motion.h1>
 
-            initial={{opacity:0,y:100}}
-            animate={{opacity:1,y:0}}
-            transition={{
-                delay:0.1
-            }}>
-                                         {/* group hovering */}
-            <Link href="#contact" className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:scale-110 hover:bg-gray-950 transition-all'>Contact me here <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition '/></Link>
-      
-        <a className='group bg-white text-black px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:scale-110 transition-all cursor-pointer border border-black/10 ' href='/CV.pdf' download={true}>Download CV <HiDownload className='opacity-60 group-hover:translate-y-1 transition'/>
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-3 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        {/* group hovering */}
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:scale-110 hover:bg-gray-950 transition-all"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition " />
+        </Link>
+
+        <a
+          className="group bg-white text-black px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:scale-110 transition-all cursor-pointer border border-black/10 "
+          href="/CV.pdf"
+          download={true}
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
 
-        <div className='flex flex-row gap-3'>
-        <a className='bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full hover:scale-[1.15] transition cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white/60' href="https://www.linkedin.com/in/chitransh-jawere-878497232/" target="_blank">
-            <BsLinkedin/>
-        </a>
+        <div className="flex flex-row gap-3">
+          <a
+            className="bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full hover:scale-[1.15] transition cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white/60"
+            href="https://www.linkedin.com/in/chitransh-jawere-878497232/"
+            target="_blank"
+          >
+            <BsLinkedin />
+          </a>
 
-        <a className='bg-white text-gray-700 text-[1.35rem] p-4 flex items-center gap-2 rounded-full hover:scale-[1.15] transition cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white/60' href="https://github.com/Chitransh-j" target="_blank">
-            <BsGithub/>
-        </a>
+          <a
+            className="bg-white text-gray-700 text-[1.35rem] p-4 flex items-center gap-2 rounded-full hover:scale-[1.15] transition cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white/60"
+            href="https://github.com/Chitransh-j"
+            target="_blank"
+          >
+            <BsGithub />
+          </a>
         </div>
-        </motion.div>
+      </motion.div>
     </section>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
